30d67f22ff449d762b34b6b93323b2f4300c3e9c
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-11-13 17:42:24 -0500

    start math

--- agent.go
+++ agent.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	_ "fmt"
+        "math/rand"
 )
+
+const numberOfAgents = 5
+const t = 3
+const q int = 2166136261
 
 type Agent struct {
 	inbox chan string
@@ -10,6 +15,26 @@ type Agent struct {
 }
 
 func newAgent() *Agent {
+        //get coeffs for polynomial
+        var polynomialCoefficients []int
+        for i:=0; i<t-1; i++ {
+            negate := rand.Int() & 1 > 0
+            randomCoefficient := rand.Intn(q-1) + 1
+            if negate {
+                randomCoefficient = -randomCoefficient
+            }
+            polynomialCoefficients = append(polynomialCoefficients, randomCoefficient)
+        }
+
+        //the secret to share is the constant term of the polynomial
+        secret := polynomialCoefficients[0]
+        // using Z mod q under addition as our cyclic group, the generator is 1
+        // in this case, the commitments are the same as the polynomial coefficients
+        var commitments []int
+        for _, coeff := range(polynomialCoefficients) {
+            commitments = append(commitments, coeff % q)
+        }
+
 	inbox := make(chan string, 10)
 	return &Agent{inbox, []*Agent{}}
 }
@@ -36,7 +61,7 @@ func (a Agent) tell(message string) {
 
 func main() {
 	var agents []*Agent
-	for i := 0; i < 5; i++ {
+	for i := 0; i < numberOfAgents; i++ {
 		agents = append(agents, newAgent())
 	}
 	for _, agent := range agents {
