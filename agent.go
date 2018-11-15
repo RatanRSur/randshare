0ae29abaf1250d762e53f0320a0a35a92d783947
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-11-14 22:41:29 -0500

    rename Add in Group interface to Times

--- agent.go
+++ agent.go
@@ -25,7 +25,7 @@ func (g ZmodQ) Inverse(n int64) int64        { return g.q - n }
 var zmodq ZmodQ = ZmodQ{q, 1}
 
 type Group interface {
-	Add(int64) int64
+	Times(int64) int64
 	Identity() int64
 	Inverse(int64) int64
 }
