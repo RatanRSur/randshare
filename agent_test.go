cc0b10b24e814857903a73f69926efb0a4340015
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-11-15 16:15:19 -0500

    formatting

--- agent_test.go
+++ agent_test.go
@@ -5,19 +5,17 @@ import (
 	"testing/quick"
 )
 
-
 func TestGroup(t *testing.T) {
 	z := ZmodQ{17, 1}
 	times := func(x int64) bool {
-		return z.Times(x, x) == mod(x + x, 17)
+		return z.Times(x, x) == mod(x+x, 17)
 	}
 	if err := quick.Check(times, nil); err != nil {
 		t.Error(err)
 	}
 
-
 	exp := func(x int64, y int64) bool {
-		return z.Exp(x, y) == mod(mod(x, 17) * mod(y, 17), 17)
+		return z.Exp(x, y) == mod(mod(x, 17)*mod(y, 17), 17)
 	}
 	if err := quick.Check(exp, nil); err != nil {
 		t.Error(err)
@@ -25,7 +23,7 @@ func TestGroup(t *testing.T) {
 	}
 
 	identity := func(x int64) bool {
-		return z.Exp(z.G, x) == mod(x, 17)
+		return z.Exp(z.G, x) == mod(x+1, 17)
 	}
 	if err := quick.Check(identity, nil); err != nil {
 		t.Error(err)
